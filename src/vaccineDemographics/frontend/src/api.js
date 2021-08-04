70625b6d14534a74bcbd56e08f40d44b04e70eeb
Will Smith <willsmithte@gmail.com>
2021-08-04 15:26:45 +1000

    usecached json

--- src/vaccineDemographics/frontend/src/api.js
+++ src/vaccineDemographics/frontend/src/api.js
@@ -1,10 +1,34 @@
+import database from './database.json';
+ 
 const BASE_URL = process.env.NODE_ENV === 'development' ?
     'http://localhost:5000' : 'someprodurl'
 
+const databaseFileName = 'database.json';
 export const api = {
     getAll: async () => {
-        const response = await fetch(`${BASE_URL}/api/areas`);
-        const jsonResponse = await response.json();
-        return jsonResponse;
+        if (isEmpty(database)) {
+            console.error('getting new')
+            const response = await fetch(`${BASE_URL}/api/areas`);
+            const jsonResponse = await response.json();
+            save(jsonResponse);
+            return jsonResponse;
+        } else {
+            console.error('used saved')
+            return database;
+        }
     }
+}
+
+function isEmpty(database) {
+    return Object.keys(database).length === 0;
+}
+
+function save(jsonData) {
+    const fileData = JSON.stringify(jsonData);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.download = `${databaseFileName}`;
+    link.href = url;
+    link.click();
 }
